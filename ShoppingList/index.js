df22eb580a6e3e40abf11ecb3799ce0e373acae1
Airstryk-1 <cj2027@yahoo.com>
2019-05-02 12:40:21 -0700

    add item button

--- ShoppingList/index.js
+++ ShoppingList/index.js
@@ -5,10 +5,36 @@ check and uncheck items on the list by clicking the "Check" button
 permanently remove items from the list */
 
 //enter items they need to purchase by entering text 
-$('.shopping-list-entry button').on('click', element => {
-    const clickedEl = $(element.target);
-    console.log(clickedEl, "word");
-});
+//$('.shopping-list-entry button').on('click', element => {
+//    const clickedEl = $(element.target);
+//    console.log(clickedEl, "word");
+//});
 
+function takeSubmit(){
+    console.log("takeSubmit");
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+        let entry = $('#shopping-list-entry').val();
+        $('#shopping-list-entry').val('');
+        $('.shopping-list').append(`
+         <li>
+            <span class="shopping-item">${entry}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                 </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+      </li>
+        `
+      );
+})};
 
+function checkFood(){
+    $('.shopping-item-toggle').click(event => {
 
+        })
+    }
+$(takeSubmit);
